c88a89e1b2a3a59a7d61fe84ca83fe152be163ae
ISHA YOUNAS <G00427804@atu.ie>
2024-12-03 21:24:52 +0000

    Completed mian method

--- WarProject/War.c
+++ WarProject/War.c
@@ -1,7 +1,8 @@
 // warnings
 #define _CRT_SECURE_NO_WARNINGS
 #include<stdio.h>
-#include <ctype.h>
+#include <string.h>
+#include <time.h>
 
 // constants
 #define PLAYERS 4
@@ -17,17 +18,16 @@ typedef struct
 typedef struct
 {
 	char name[100];
-	card game[ROUNDS];
+	card hamd[ROUNDS]; // card struct
 	int score;
 }player; // struct - player
 
-
 // function prototypes
-void initializePLayer(player* players, int numPlayers);
-void shuffleCards(int* deck, int size);
-void dealCards(player* players, int numPlayers);
+void initializeDeck(card* deck);
+void shuffleCards(card* deck);
+void dealCards(player* players, card* pack, int numPlayers);
 void playRound(player* players, int numPlayers, int round);
-int winner(int* playedCards, int numPlayers);
+int findWinner(int* playedCards, int numPlayers);
 void saveGame(player* players, int numPlayers, int currentRound);
 int loadGame(player* players, int* numPlayers, int* currentRound);
 void displayGame(player* players, int numPlayers, int currentRound);
@@ -36,44 +36,99 @@ int menu();
 void main()
 {
 	// variables
-	player players;
-	card cards;
+	player players[PLAYERS];
+	card pack[52];
 	int i;
 	int numPlayers;
 	int choice;
 	int currentRound = 0;
+	char startGame[20];
 
 	printf("Welcome to the Card Game 'War'\n");
 
-	do 
+	// loading/starting  game
+	printf("1. New Game\n2. Load Game\n");
+	scanf("%d", &choice);
+
+	if (choice == 2)
 	{
-		printf("Enter the number of players playing (2-4): ");
-		scanf("%d", &numPlayers);
+		// trying to load the game
+		if (!loadGame(players, &numPlayers, &currentRound))
+		{
+			printf("No saved game found!\n");
 
-		if (numPlayers < 2 || numPlayers > 4) {
-			printf("There must be 2-4 players playing this game.\n");
-		} // while
+			// asking the user if they want to start a new game
+			printf("Would you like to start a new game? (enter y or n)");
+			scanf("%s", startGame);
+			if (startGame == 'y' || startGame == 'Y')
+			{
+				choice = 1;
+			} // if
 
-	} while (numPlayers < 2 || numPlayers > 4); // do-while
+			else //(startGame == 'n' || startGame == 'N')
+			{
+				choice = 0;
+			} // else 
+		} // if
+	} // if
 
-	for (i = 0; i < numPlayers; i++)
+	if (choice == 1)
 	{
-		printf("Enter the name for player %d: ", i + 1);
-		scanf("%s", &players.name);
-	} // for
+		do
+		{
+			// prompting the user for input
+			printf("Enter the number of players playing (2-4): ");
+			scanf("%d", &numPlayers);
+
+			if (numPlayers < 2 || numPlayers > 4) {
+				printf("There must be 2-4 players playing this game.\n");
+			} // while
+
+		} while (numPlayers < 2 || numPlayers > 4); // do-while
+
+		// initializing each player with a for loop
+		for (i = 0; i < numPlayers; i++)
+		{
+			printf("Enter the name for player %d: ", i + 1);
+			scanf("%s", &players[i].name);
+			players[i].score = 0;
+		} // for
+	} // if	
+
+	// calling the methods
+	initializeDeck(pack);
+	shuffleCards(pack);
+	dealCards(players, pack, numPlayers);
+
+	// looping the game
+	while (currentRound < ROUNDS)
+	{
+		printf("\n----Round %d ---\n", currentRound);
+		playRound(players, numPlayers, currentRound);
+		currentRound++;
+
+		saveGame(players, numPlayers, currentRound);
+	} // while
+
+	// winner variable calling the findWinner method
+	int winner = findWinner(players, numPlayers);
+
+	// displaying the winner
+	printf("\n--- Game Over ---\n");
+	printf("The winner is %s\n", players[winner].name);
 } // main
 
-void initializePLayer(player* players, int numPlayers)
+void initializeDeck(card* deck)
 {
 
-} // initializePLayer
+} // initializeDeck
 
-void shuffleCards(int* deck, int size)
+void shuffleCards(card* deck)
 {
 
 } // shuffleCards
 
-void dealCards(player* players, int numPlayers)
+void dealCards(player* players, card* deck, int numPlayers)
 {
 
 } // dealCards
@@ -83,10 +138,10 @@ void playRound(player* players, int numPlayers, int round)
 
 } // playRound
 
-int winner(int* playedCards, int numPlayers)
+int findWinner(int* playedCards, int numPlayers)
 {
 
-} // winner
+} // findWinner
 
 void saveGame(player* players, int numPlayers, int currentRound)
 {
